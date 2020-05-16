d44389d414122906e67d323a864f5beda5a27475
Jos Verlinde <jos_verlinde@hotmail.com>
2020-05-17 01:18:34 +0200

    ulab in cmodules folder

--- ports/esp32/boards/JOSV_SPIRAM/mpconfigboard.mk
+++ ports/esp32/boards/JOSV_SPIRAM/mpconfigboard.mk
@@ -1,5 +1,6 @@
+#compile for newer chips only, and avoid warning
+CONFIG_IDF_FIRMWARE_CHIP_ID=0x0001
+
 SDKCONFIG += boards/sdkconfig.base
 SDKCONFIG += boards/sdkconfig.spiram
 
-#compile for newer chips only, and avoid warning
-CONFIG_IDF_FIRMWARE_CHIP_ID=0x0001
