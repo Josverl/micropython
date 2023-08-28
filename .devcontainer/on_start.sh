b8751fbe0e52999cdd4f35b0c8ba480f6a8797f1
Jos Verlinde <jos_verlinde@hotmail.com>
2023-08-28 07:59:30 +0000

    initial devcontainer
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

---
+++ .devcontainer/on_start.sh
@@ -0,0 +1,7 @@
+# Use autobuild setup from github actions
+source tools/ci.sh
+
+# declare -F| grep ci_.*_setup
+
+echo "To list defined functions type: declare -F |  declare -F| grep ci_.*_build"
+
