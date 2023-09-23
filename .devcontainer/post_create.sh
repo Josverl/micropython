40d5f903682fbbbd165bb41e120fa8c91dfde9ff
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-09-23 21:31:53 +0000

    codespaces: Add welcome message.
    Signed-off-by: Jos Verlinde <Jos.Verlinde@Microsoft.com>

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,15 +2,14 @@ sudo DEBIAN_FRONTEND=noninteractive apt update -y
 
 echo "# always load micropython ci.sh" >> ~/.bashrc
 echo "source tools/ci.sh" >> ~/.bashrc
+echo "To list defined functions type: declare -F |  declare -F| grep ci_.*_build"
 
 # simple menu to setup for build
-pip install pick
-python3 .devcontainer/setup_picker.py
-
-
+pip install -U pick
+# pre-commit
+pip install -U pre-commit
+pre-commit install --hook-type pre-commit --hook-type commit-msg
 
 # Use autobuild setup from github actions
 source tools/ci.sh
-declare -F| grep ci_.*_build
-echo "To list defined functions type: declare -F |  declare -F| grep ci_.*_build"
 
