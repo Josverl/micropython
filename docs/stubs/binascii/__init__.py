edbd5cac15b873e69455885eb0119e5e1db0fc9b
Jim Mussared <jim.mussared@gmail.com>
2024-07-13 12:35:30 +0200

    docs/stubs/binascii: Move binascii docs to use stubs.
    Signed-off-by: Jim Mussared <jim.mussared@gmail.com>

--- docs/stubs/binascii/__init__.py
+++ docs/stubs/binascii/__init__.py
@@ -1,45 +1,83 @@
 """
-binary/ASCII conversions. See: https://docs.micropython.org/en/latest/library/binascii.html
-
-|see_cpython_module| :mod:`python:binascii` https://docs.python.org/3/library/binascii.html .
-
 This module implements conversions between binary data and various
 encodings of it in ASCII form (in both directions).
 """
 
-# source version: latest
-# origin module:: /home/jimmo/src/github.com/micropython/micropython3/docs/library/binascii.rst
-from typing import Any, Optional
+from annotations import module_availability, cpython_stdlib, Level, Buffer
 
-def hexlify(data, sep: Optional[Any] = None) -> bytes:
+cpython_stdlib("binascii")
+module_availability(Level.EXTRA_FEATURES)
+
+def hexlify(data: Buffer, sep: Buffer = None, /) -> bytes:
     """
-    Convert the bytes in the *data* object to a hexadecimal representation.
-    Returns a bytes object.
+    Convert an object implementing the `buffer protocol` to a hexadecimal
+    representation.
 
-    If the additional argument *sep* is supplied it is used as a separator
-    between hexadecimal values.
+    Arguments:
+        data:
+            An object implementing the :term:`buffer protocol` (e.g. `bytes`
+            or `bytearray`).
+        sep:
+            Optional separator between each pair of nibbles. Must be a `bytes`
+            of length 1.
+
+    Returns:
+        A `bytes` instance containing a string of hexadecimal nibbles.
+
+    Notes:
+        Consider using `bytes.hex` instead (also available as `bytearray.hex`
+        and `memoryview.hex`).
     """
     ...
 
-def unhexlify(data) -> bytes:
+def unhexlify(data: Buffer, /) -> bytes:
     """
     Convert hexadecimal data to binary representation. Returns bytes string.
     (i.e. inverse of hexlify)
+
+    ``unhexlify`` does not support separator-delimited inputs. Use
+    `str.replace` to remove separators first.
+
+    Arguments:
+        data:
+            An object implementing the :term:`buffer protocol` (e.g. `bytes`
+            or `bytearray`) containing the hexadecimal-encoded nibbles.
+
+    Returns:
+        A `bytes` instance containing the decoded data.
+
+    Notes:
+        Consider using `bytes.fromhex` instead.
     """
     ...
 
-def a2b_base64(data) -> bytes:
+def b2a_base64(data: Buffer, /, newline=True) -> bytes:
+    """
+    Encode binary data in base64 format, as in `RFC 3548 <https://tools.ietf.org/html/rfc3548.html>`_.
+
+    Arguments:
+        data:
+            An object implementing the :term:`buffer protocol` (e.g. `bytes`
+            or `bytearray`).
+        newline:
+            If set to ``True``, appends a newline to the end of the output.
+
+    Returns:
+        A `bytes` instance containing the base64-encoded data.
+    """
+    ...
+
+def a2b_base64(data: Buffer, /) -> bytes:
     """
     Decode base64-encoded data, ignoring invalid characters in the input.
     Conforms to `RFC 2045 s.6.8 <https://tools.ietf.org/html/rfc2045#section-6.8>`_.
-    Returns a bytes object.
-    """
-    ...
 
-def b2a_base64(data, *, newline=True) -> bytes:
-    """
-    Encode binary data in base64 format, as in `RFC 3548
-    <https://tools.ietf.org/html/rfc3548.html>`_. Returns the encoded data
-    followed by a newline character if newline is true, as a bytes object.
+    Arguments:
+        data:
+            An object implementing the :term:`buffer protocol` (e.g. `bytes`
+            or `bytearray`) containing the base64-encoded data.
+
+    Returns:
+        A `bytes` instance containing the decoded data.
     """
     ...
