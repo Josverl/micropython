5115f2a1da6b0ce833a53b6e13c22e7e686c9a28
Jos Verlinde <jos_verlinde@hotmail.com>
2025-04-27 23:08:53 +0200

    Tests: Enhance test script to include coverage reporting.
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- tools/mpremote/tests/run-mpremote-tests.sh
+++ tools/mpremote/tests/run-mpremote-tests.sh
@@ -8,15 +8,39 @@ if [ -z "$1" ]; then
     # Find tests matching test_*.sh
     TESTS=${TEST_DIR}/test_*.sh
 else
-    # Specific test path from the command line.
-    TESTS="$1"
+    COVERAGE=false
+    TESTS=()
+    for arg in "$@"; do
+        if [ "$arg" == "--coverage" ]; then
+            COVERAGE=true
+        else
+            TESTS+=("$arg")
+        fi
+    done
+    if [ ${#TESTS[@]} -eq 0 ]; then
+        TESTS=${TEST_DIR}/test_*.sh
+    fi
+fi
+
+if [ "$COVERAGE" = true ]; then
+    # Check if coverage is installed
+    if ! command -v coverage &> /dev/null; then
+        echo "Coverage is not installed. Please install it to run tests with coverage."
+        exit 1
+    fi
+    echo "Start coverage"
+    coverage erase
 fi
 
 for t in $TESTS; do
     TMP=$(mktemp -d)
     echo -n "${t}: "
+    if [ "$COVERAGE" = true ]; then
+        MPREMOTE="coverage run --append --context=${t} ${TEST_DIR}/../mpremote.py"
+        echo ${MPREMOTE}
+    fi
     # Strip CR and replace the random temp dir with a token.
-    if env MPREMOTE=${MPREMOTE} TMP="${TMP}" "${t}" 2>&1 | tr -d '\r' | sed "s,${TMP},"'${TMP},g' > "${t}.out"; then
+    if env MPREMOTE="${MPREMOTE}" TMP="${TMP}" "${t}" 2>&1 | tr -d '\r' | sed "s,${TMP},"'${TMP},g' > "${t}.out"; then
         if diff "${t}.out" "${t}.exp" > /dev/null; then
             echo "OK"
         else
@@ -28,3 +52,10 @@ for t in $TESTS; do
     fi
     rm -r "${TMP}"
 done
+
+if [ "$COVERAGE" = true ]; then
+    # Generate coverage reports
+    coverage report -m 
+    coverage html --show-contexts
+    coverage xml
+fi
