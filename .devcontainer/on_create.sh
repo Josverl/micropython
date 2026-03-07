2380d228e62250a9336ae78d2e6172fb761f8e4d
Jos Verlinde <jos_verlinde@hotmail.com>
2026-03-07 23:01:25 +0000

    devcontainer: Add menu to install toolchains.
    Signed-off-by: Jos Verlinde <Jos.Verlinde@Microsoft.com>
    Signed-off-by: Jos Verlinde <jos_verlinde@hotmail.com>

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -1,8 +1,16 @@
 sudo DEBIAN_FRONTEND=noninteractive apt update -y
+
+echo "# always load micropython ci.sh" >> ~/.bashrc
+echo "source tools/ci.sh" >> ~/.bashrc
+
+# simple menu to setup for build
+pip install pick
+python3 .devcontainer/setup_picker.py
+
+
+
 # Use autobuild setup from github actions
 source tools/ci.sh
-
-declare -F| grep ci_.*_setup
-
+declare -F| grep ci_.*_build
 echo "To list defined functions type: declare -F |  declare -F| grep ci_.*_build"
 
