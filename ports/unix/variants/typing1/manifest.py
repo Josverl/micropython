cd3a571abfd9999bfe8f1a47e42fc3185d0f843f
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-05-28 21:46:28 +0200

    variant 1:  Add typing mod from micropython-stubs repo.
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- ports/unix/variants/typing1/manifest.py
+++ ports/unix/variants/typing1/manifest.py
@@ -2,14 +2,8 @@ include("$(PORT_DIR)/variants/manifest.py")
 
 include("$(MPY_DIR)/extmod/asyncio")
 
-freeze_as_mpy("typing", opt=3)
-freeze_as_mpy("typing_extensions", opt=3)
 
+# Freeze typing modules from local micropython-stubs repo
 require("__future__", opt_level=3)
-require("abc", opt_level=3)
+module("typing.py", base_path="/home/jos/micropython-stubs/mip", opt=3)
 
-# belt and suspenders - collections is already included 
-require("collections", opt_level=3)
-
-# does not exist yet
-# require("collections.abc")
