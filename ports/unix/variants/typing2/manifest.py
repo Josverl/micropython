7d390d45c5dfb3fddde09ea0788b5f921efedc9f
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-05-30 16:41:09 +0200

    typing: Add micropython variant.
    Uses the typing module from micropyton-stubs.
    
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

---
+++ ports/unix/variants/typing2/manifest.py
@@ -0,0 +1,8 @@
+include("$(PORT_DIR)/variants/manifest.py")
+
+include("$(MPY_DIR)/extmod/asyncio")
+
+# to reduce code size:
+#  - default optimization level is 3
+#  - extensions are disabled by default
+require("bundle-typing", extensions=False)
