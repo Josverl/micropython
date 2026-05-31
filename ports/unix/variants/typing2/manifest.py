8a43cd24494114945e5ea21c92941af15233405f
Jos Verlinde <Jos_Verlinde@hotmail.com>
2026-06-01 01:36:42 +0200

    fixup: Update manifest.py files for typing variants
    Signed-off-by: Jos Verlinde <Jos_Verlinde@hotmail.com>

--- ports/unix/variants/typing2/manifest.py
+++ ports/unix/variants/typing2/manifest.py
@@ -1,3 +1,7 @@
+TYPE_CHECKING = False
+if TYPE_CHECKING:
+    from manifestfile import *
+
 include("$(PORT_DIR)/variants/manifest.py")
 
 include("$(MPY_DIR)/extmod/asyncio")
@@ -6,3 +10,12 @@ include("$(MPY_DIR)/extmod/asyncio")
 #  - default optimization level is 3
 #  - extensions are disabled by default
 require("bundle-typing", extensions=False)
+
+# BEST SO FAR:
+# freeze_as_str("$(MPY_LIB_DIR)/python-stdlib/__future__")
+
+#require("typing", opt_level=3) # 2_112
+# require("__future__", opt_level=3) # 664
+# require("future", opt_level=3) # 664
+# module( "__future__.py", "$(MPY_LIB_DIR)/python-stdlib/__future__", opt=3) # 608 bytes
+# freeze_as_mpy("$(MPY_LIB_DIR)/python-stdlib/__future__", script = "__future__.py", opt=3) # 608 bytes
--- ports/unix/variants/typing2b/manifest.py
+++ ports/unix/variants/typing2b/manifest.py
@@ -2,7 +2,6 @@ include("$(PORT_DIR)/variants/manifest.py")
 
 include("$(MPY_DIR)/extmod/asyncio")
 
-require("collections", opt_level=3)
 # to reduce code size:
 #  - default optimization level is 3
 #  - extensions are disabled by default
